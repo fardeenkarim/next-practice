60794ea6278d6fc73c676290f83c7138bef26d67
Fardeen Karim <happisaas@gmail.com>
2024-04-16 08:44:10 +0600

    UPDATED

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="mt-8 ml-8 text-8xl">
-      <div>This is Home Page</div>
+      <div>This is About Page</div>
     </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="pt-8 pl-8">
+        <Link href="/" className='bg-neutral-950 text-white p-5 rounded-md text-xl mr-5'>Home</Link>
+        <Link href="/about" className='bg-neutral-950 text-white p-5 rounded-md text-xl mr-5'>About</Link>
+        <Link href="/service" className='bg-neutral-950 text-white p-5 rounded-md text-xl mr-5'>Service</Link>
+        <Link href="/portfolio" className='bg-neutral-950 text-white p-5 rounded-md text-xl mr-5'>Portfolio</Link>
+        <Link href="/blog" className='bg-neutral-950 text-white p-5 rounded-md text-xl mr-5'>Blog</Link>
+          <Link href="/contact" className='bg-neutral-950 text-white p-5 rounded-md text-xl mr-5'>Contact</Link>
+          </div>
+        {children}
+      </body>
     </html>
   );
 }
